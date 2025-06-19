bb2977de754dbc1f2b8ef06c2c0b96ebe8386950
Jahaira Flores <jahaira@umich.edu>
2025-06-19 00:50:04 -0500

    updating login style

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,8 +5,15 @@ import './LoginPage.css';
 const LoginPage = () => {
   return(
     <div className="login_container">
-      <h1>Login</h1>
-      <LoginForm/>
+      <div className="left_container">
+        <h1>Welcome back</h1>
+        <p> Please enter your details</p>
+        <LoginForm/>
+      </div>
+
+      <div className='right_container'>
+        <img src="assets/login_dog_page"/>
+      </div>
     </div>
   );
 };
